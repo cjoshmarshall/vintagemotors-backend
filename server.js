b75889ba319184587e584bb4198da5a36e52e8e8
cjoshmarshall <cjoshmarshall@gmail.com>
2022-04-29 21:34:36 +0530

    fix 1.7

--- server.js
+++ server.js
@@ -26,14 +26,6 @@ app.use('/api/tariff/',require('./routes/tariffRoute'))
 app.use('/api/comments/',require('./routes/commentRoute'))
 app.use('/api/orders/',require('./routes/bookingRoute'))
 
-const path=require("path")
-
-if (process.env.NODE_ENV === "production") {
-    app.use("/",express.static("client/build"));
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-    });
-  }
 
 app.get('/',(req,res)=>
     res.send('Connected')
