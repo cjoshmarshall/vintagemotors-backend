72e749dc2dfb0c60e03d9e2089ac8a3032aec01d
Josh Marshall <cjoshmarshall@gmail.com>
2022-12-27 13:08:00 +0530

    Handler Export Fix

--- src/handler.js
+++ src/handler.js
@@ -5,9 +5,11 @@
 
 // module.exports.handler = serverless(app);
 
-
-const awsServerlessExpress = require('aws-serverless-express')
+const awsServerlessExpress = require("aws-serverless-express");
 const app = require("./index");
-const server = awsServerlessExpress.createServer(app)
 
-exports.handler = (event, context) => awsServerlessExpress.proxy(server, event, context)
+const server = awsServerlessExpress.createServer(app);
+
+exports.handler = (event, context) => {
+  return awsServerlessExpress.proxy(server, event, context);
+};
